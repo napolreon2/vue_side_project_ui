d486e96311fa57e14f6862457f7cf06c8a904899
MinsupLee0715 <sswdndn@gmail.com>
2021-08-31 16:09:11 +0900

    Inspire 페이지 복제 default.vue에 empire 링크 추가

---
+++ pages/empire.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;밥이 없으면 고기를 먹어라&#8221;
+        <footer>
+          <small>
+            <em>&mdash;이 민섭</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
