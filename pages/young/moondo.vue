b6c105b1d6b99c0318328969ab8399c847770930
MinsupLee0715 <sswdndn@gmail.com>
2021-08-31 18:31:29 +0900

    Enjoy v-for Moondo script

---
+++ pages/young/moondo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1><nuxt-link to="/young/moondo_detail">클릭하여 상세한 내용을 확인해주세요.</nuxt-link></h1>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style>
+
+</style>
---
+++ pages/young/moondo_detail.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <input v-model="text" class="input">
+    <v-btn name="add_butotn" @click="add(); add_list();" class="add_button"
+      elevation="4"
+    >
+      ADD
+    </v-btn>
+    <div v-for="i in list" :key="i">
+      ({{i}}) 문도, 가고 싶은 데로 간다    &emsp;    아니다, &emsp;  문도, 가고 싶은대로 간다
+    </div>
+    
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        text: 1,
+        list: [1]
+      }
+    },
+    methods: {
+      add: function () {
+        this.text = this.text + 1
+      },
+      add_list: function () {
+        this.list.push(this.text)
+        console.log(this.list)
+      }
+    }
+  }
+</script>
+
+<style>
+.input {
+  color: white;
+}
+
+v-btn.add_button {
+  color: white;
+  background-color: lightsteelblue;
+  display: inline-block;
+}
+</style>
